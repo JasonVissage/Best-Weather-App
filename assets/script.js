8628693862f752bd2844823b3ff52336031f4d5d
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2021-12-15 19:54:09 -0600

    add functions for both api's. Console.log the city searched and brings up lat and long.

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,63 @@
 var searchBtnEl = document.querySelector("#searchBtn");
 var searchFieldEl = document.querySelector("#searchField");
 var displayEl = document.querySelector("#display");
-var cityNameEl = document.querySelector("#cityName");
+var cityEl = document.querySelector("#cityEl");
 var tempEl = document.querySelector("#temp");
 var windEl = document.querySelector("#wind");
 var humidEl = document.querySelector("#humid");
 var uvEl = document.querySelector("#uv");
-var citySearchEl = document.querySelector("#citySearch")
+var citySearchFormEl = document.querySelector("#citySearchForm");
+var currentDay = new Date;
 
 // api add in's
-var cityName = "Dallas"
-var lat = "32.7767"
-var long = "-96.7970"
+// var cityName = "Dallas"
+// var lat = "32.7767"
+// var long = "-96.7970"
 
-// api Key for OneCall
+// api info
 var apiKey = "09fdfda92faacaa82f0791b49a497abf"
-var apiOneUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apiKey
-var apiCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey
 
 
-fetch(apiOneUrl)
+
+// pulls weather in
+var getWeather = function() {
+    var apiOneUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + apiKey
+    fetch(apiOneUrl)
+    .then(res => {
+        if (res.ok) {
+            res.json()
+            .then(function(city, lat, long) {
+                // put city, lat and lon in parameters in function. 
+                // plug in getWeather into getCity
+                // plug in getCity into click event
+                }
+            )
+        }
+    })
+};
+
+
+// pulls city
+var getCity = function() {
+    var apiCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey
+    fetch(apiCurrentUrl)
     .then(res => {
         if (res.ok) {
             res.json()
             .then(function(data) {
-                console.log(data)
+            console.log(data.coord.lon)
+
+            
             })
         }
     })
+}
+getCity()
+// gets city
+var searchedCity = function(event) {
+    event.preventDefault();
+    var city = searchFieldEl.value.trim();
+    console.log(city)
+}
 
-fetch(apiCurrentUrl)
-.then(res => {
-    if (res.ok) {
-        res.json()
-        .then(function(data) {
-            console.log(data)
-        })
-    }
-})
+searchBtnEl.addEventListener('click', searchedCity);
